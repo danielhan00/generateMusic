a420dba165a8a04a0f6f6478a20ce32144b3606a
gcoop327 <gcoop327@gmail.com>
2022-02-18 15:24:57 -0500

    slightly more live frontend

--- midiparser-frontend/src/components/LiveInfoBar/LiveInfoBar.js
+++ midiparser-frontend/src/components/LiveInfoBar/LiveInfoBar.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { CounterTextField } from '../reusable/CounterTextField/CounterTextField';
 import { DropdownMenu } from '../reusable/DropdownMenu/DropdownMenu';
+import { TimeSignatureInput } from '../reusable/TimeSignatureInput/TimeSignatureInput';
 import './LiveInfoBar.css';
 
 export const LiveInfoBar =(props)=>{
@@ -14,6 +15,10 @@ export const LiveInfoBar =(props)=>{
                 <td className="InfoField">
                     <text>Tempo</text>
                     <CounterTextField input={props.tempo}  min='50' max='300'></CounterTextField>
+                </td>
+                <td className="InfoField">
+                    <text>Time Signature</text>
+                    <TimeSignatureInput input={props.tempo}></TimeSignatureInput>
                 </td>
                 <td className="InfoField">
                     <text>Time Signature</text>
