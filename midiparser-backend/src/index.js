b0bd2512ae045b9a28b64ea4269d16abb33f5cf7
kiaraoliver <oliver.ki@northeastern.edu>
2022-03-28 12:59:32 -0400

    added parseBars function and noteDictionary function

--- midiparser-backend/src/index.js
+++ midiparser-backend/src/index.js
@@ -1,15 +1,28 @@
 const { detect } = require('@tonaljs/chord-detect')
+const { Pcset } = require('@tonaljs/tonal')
 const MidiConvert = require('midiconvert')
 const { Midi, Interval, Note, Scale } = require('@tonaljs/tonal')
+
+var key = ''
+var bpm = 0
+var timeSignature = []
+var meausureDuration
+var notes = []
 
 let fs = require('fs')
 fs.readFile('testmid.mid', 'binary', function (err, midiBlob) {
   if (!err) {
-    var midi = MidiConvert.parse(midiBlob)
+    midi = MidiConvert.parse(midiBlob)
     notes = midi.tracks[1].notes
     bpm = midi.header.bpm
-    numberToNoteNames(notes)
-    console.log(notes)
+    timeSignature = midi.header.timeSignature
+    meausureDuration = (240 / bpm) * (timeSignature[0] / timeSignature[1])
+    bars = parseBars(notes, meausureDuration)
+    notesInBar = noteDictionary(bars)
+    //console.log(meausureDuration)
+    //console.log(numberToNoteNames(notes))
+  } else {
+    console.log('error')
   }
 })
 
@@ -28,4 +41,50 @@ function numberToNoteNames(noteArray) {
   return notes
 }
 
-console.log(notesInKey('C major'))
+function noteDictionary(bars) {
+  let dict = new Object()
+  for (const [key, value] of Object.entries(bars)) {
+    dict[key] = numberToNoteNames(value)
+    //console.log(key, dict[key])
+  }
+  return dict
+}
+
+function numberToNoteNames(noteArray) {
+  const notes = []
+  for (var i = 0; i < noteArray.length; i++) {
+    const noteName = Midi.midiToNoteName(noteArray[i].midi)
+    //console.log(noteArray[i])
+    notes.push(noteName)
+  }
+
+  return notes
+}
+function parseBars(noteArray, duration) {
+  let dict = new Object()
+  let bar = duration
+  let count = 1
+  let currBar = []
+
+  for (var i = 0; i < noteArray.length; i++) {
+    if (noteArray[i].time < bar) {
+      currBar.push(noteArray[i])
+      if (noteArray[i].time + noteArray[i].duration > bar) {
+        dict[count] = currBar
+        currBar = []
+        count++
+        currBar.push(noteArray[i])
+        bar += duration
+      }
+    } else {
+      dict[count] = currBar
+      currBar = []
+      count++
+      currBar.push(noteArray[i])
+      bar += duration
+    }
+  }
+  dict[count] = currBar
+
+  return dict
+}
